4cb8568eba9555d557563b2de08fa5bd1478f065
Taiki Endo <te316e89@gmail.com>
2019-11-13 20:19:34 +0900

    Update html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -204,7 +204,7 @@
 //!
 //! [futures-await]: https://github.com/alexcrichton/futures-await
 
-#![doc(html_root_url = "https://docs.rs/futures-async-stream/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/futures-async-stream/0.1.1")]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms, single_use_lifetimes), allow(dead_code))
