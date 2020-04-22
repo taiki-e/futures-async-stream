a1ab12213fa4277fed6b0635775c8662715c963b
Taiki Endo <te316e89@gmail.com>
2020-04-22 18:55:23 +0900

    Add no-std test

---
+++ tests/no-std/src/lib.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![feature(generators)]
+
+use futures_async_stream::async_stream;
+
+#[async_stream(item = T)]
+async fn iter<T>(iter: impl IntoIterator<Item = T>) {
+    for x in iter {
+        yield x;
+    }
+}
+
+#[async_stream(item = i32)]
+pub async fn stream() {
+    let mut cnt = 0;
+    #[for_await]
+    for x in iter(1..4) {
+        cnt += x;
+        yield x;
+    }
+    yield cnt;
+}
