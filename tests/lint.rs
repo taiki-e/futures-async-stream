72537ff4f1605fa3cdfbd85d89795416e420f57e
Taiki Endo <te316e89@gmail.com>
2020-09-24 06:49:37 +0900

    Update lint test

--- tests/lint.rs
+++ tests/lint.rs
@@ -1,8 +1,6 @@
 #![allow(incomplete_features)]
 #![feature(generators, impl_trait_in_bindings)]
-#![warn(rust_2018_idioms, single_use_lifetimes)]
-#![warn(future_incompatible, nonstandard_style, rust_2018_compatibility, unused)]
-#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+#![warn(future_incompatible, nonstandard_style, rust_2018_compatibility, rust_2018_idioms, unused)]
 #![forbid(unsafe_code)]
 #![allow(unknown_lints)] // for old compilers
 #![warn(
@@ -38,7 +36,11 @@
 // unused_crate_dependencies: unrelated
 // unsafe_code: forbidden
 // unsafe_block_in_unsafe_fn: unstable
-// unstable_features: we depend on this
+// unstable_features: deprecated: https://doc.rust-lang.org/beta/rustc/lints/listing/allowed-by-default.html#unstable-features
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+#![warn(clippy::restriction)]
+#![allow(clippy::blanket_clippy_restriction_lints)] // this is a test, so enable all restriction lints intentionally.
+#![allow(clippy::implicit_return, clippy::let_underscore_must_use)]
 
 // Check interoperability with rustc and clippy lints.
 
@@ -50,6 +52,7 @@ pub mod basic {
 }
 
 #[allow(box_pointers)]
+#[allow(clippy::restriction)]
 #[test]
 fn check_lint_list() {
     use std::{env, process::Command, str};
