2009ac747f0c17c4f2a3a0b4192f6a04a4e0b976
Taiki Endo <te316e89@gmail.com>
2020-10-30 03:22:30 +0900

    Do not require developers to manually set TRYBUILD

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,7 +1,13 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 
+use std::env;
+
 #[test]
 fn ui() {
+    if !env::var_os("CI").map_or(false, |v| v == "true") {
+        env::set_var("TRYBUILD", "overwrite");
+    }
+
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 }
