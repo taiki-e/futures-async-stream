7d5443ff69b07c16541ea2ad384d614e7751b9d2
Taiki Endo <te316e89@gmail.com>
2020-11-08 21:04:43 +0900

    Remove needless derive

--- futures-async-stream-macro/src/visitor.rs
+++ futures-async-stream-macro/src/visitor.rs
@@ -14,7 +14,7 @@ use crate::{
 /// The scope in which `#[for_await]`, `.await`, or `yield` was called.
 ///
 /// The type of generator depends on which scope is called.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub(crate) enum Scope {
     /// `async fn`, `async {}`, or `async ||`
     Future,
