2e69db27af9c581677149c9b3131d08ea8b92b66
Taiki Endo <te316e89@gmail.com>
2021-08-27 09:02:33 +0900

    Reduce instantiations of syn::Error::new_spanned

--- futures-async-stream-macro/src/utils.rs
+++ futures-async-stream-macro/src/utils.rs
@@ -7,8 +7,8 @@ use syn::{
 };
 
 macro_rules! format_err {
-    ($span:expr, $msg:expr) => {
-        syn::Error::new_spanned(&$span, $msg)
+    ($span:expr, $msg:expr $(,)?) => {
+        syn::Error::new_spanned(&$span as &dyn quote::ToTokens, &$msg as &dyn std::fmt::Display)
     };
     ($span:expr, $($tt:tt)*) => {
         format_err!($span, format!($($tt)*))
