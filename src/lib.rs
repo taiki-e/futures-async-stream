1cd9a9afe7e389ca149d74acbc4a0effc1d8c62f
Taiki Endo <te316e89@gmail.com>
2022-05-25 20:22:50 +0900

    Add safety comments to unsafe impls
    clippy::undocumented_unsafe_blocks lint can now detect this.

--- src/lib.rs
+++ src/lib.rs
@@ -294,7 +294,10 @@ mod future {
     #[derive(Debug, Copy, Clone)]
     pub struct ResumeTy(pub(crate) NonNull<Context<'static>>);
 
+    // SAFETY: `Context` is `Send + Sync` and the caller of the `get_context` function
+    // that dereferences a pointer must guarantee that no data races will occur.
     unsafe impl Send for ResumeTy {}
+    // SAFETY: see `Send` impl
     unsafe impl Sync for ResumeTy {}
 
     /// Wrap a generator in a future.
