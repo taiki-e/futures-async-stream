ef1ecb1d7e6f5b7af7bbee248879730b8418a991
Taiki Endo <te316e89@gmail.com>
2023-12-05 22:24:33 +0900

    codegen: Update assert_impl helpers

--- tools/codegen/src/main.rs
+++ tools/codegen/src/main.rs
@@ -273,10 +273,10 @@ fn gen_assert_impl() -> Result<()> {
             use core::marker::PhantomPinned;
             /// `Send` & `!Sync`
             #[allow(dead_code)]
-            struct NotSync(core::cell::Cell<()>);
+            struct NotSync(core::cell::UnsafeCell<()>);
             /// `!Send` & `!Sync`
             #[allow(dead_code)]
-            struct NotSendSync(std::rc::Rc<()>);
+            struct NotSendSync(*const ());
             /// `!UnwindSafe`
             #[allow(dead_code)]
             struct NotUnwindSafe(&'static mut ());
