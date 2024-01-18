20d9863b6364cb1ad5ecee90380113124e9c5425
Taiki Endo <te316e89@gmail.com>
2024-01-18 23:19:07 +0900

    Update ui test output to nightly-2024-01-18

--- tests/ui/unresolved-type.rs
+++ tests/ui/unresolved-type.rs
@@ -4,7 +4,7 @@
 
 use futures_async_stream::stream;
 
-#[stream(item = L)] //~ ERROR cannot find type `Left` in this scope
+#[stream(item = L)] //~ ERROR cannot find type `L` in this scope
 async fn foo() {}
 
 fn main() {}
