5f3908a9c2ff1d5eb1d0c9faee9c8a846dc2dccc
Taiki Endo <te316e89@gmail.com>
2024-06-15 23:44:36 +0900

    tests: Ignore dead_code lint

--- tests/elisions.rs
+++ tests/elisions.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-#![allow(clippy::needless_pass_by_value)]
+#![allow(dead_code, clippy::needless_pass_by_value)]
 #![feature(coroutines)]
 
 use futures_async_stream::stream;
